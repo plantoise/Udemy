5f472ab46db846096efbdfbd1af51d7c22c0478f
plantoise <gunnar.mar.j@gmail.com>
2024-04-29 16:54:52 +0000

    added quiz game

---
+++ Quiz Game/question_model.py
@@ -0,0 +1,6 @@
+# Create a Question class with an __init__() methood with two attrributes: text and answer attributes.
+
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
